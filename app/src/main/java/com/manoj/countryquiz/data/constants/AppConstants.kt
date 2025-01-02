0d8ebf489b5c144be2adb407f9f772edc40857d6
Manoj Perumarath <manoj.pr@emvigotech.com>
2025-01-02 16:57:05 +0530

    minor bug fixes

--- app/src/main/java/com/manoj/countryquiz/data/constants/AppConstants.kt
+++ app/src/main/java/com/manoj/countryquiz/data/constants/AppConstants.kt
@@ -4,7 +4,7 @@ object  AppConstants {
 
     object DefaultTime{
 //        const val CHALLENGE_START_TIME = 20000L
-        const val CHALLENGE_START_TIME = 6000L
-        const val CHALLENGE_ANSWER_TIME = 3000L
+        const val CHALLENGE_START_TIME = 20000L
+        const val CHALLENGE_ANSWER_TIME = 10000L
     }
 }
--- app/src/main/java/com/manoj/countryquiz/ui/screen/QuizScoreFragment.kt
+++ app/src/main/java/com/manoj/countryquiz/ui/screen/QuizScoreFragment.kt
@@ -1,14 +1,20 @@
 package com.manoj.countryquiz.ui.screen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.manoj.countryquiz.R
+import com.manoj.countryquiz.databinding.FragmentQuizScoreBinding
+import com.manoj.countryquiz.ui.viewmodel.MainViewModel
 
 
 class QuizScoreFragment : Fragment() {
+    private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var binding: FragmentQuizScoreBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +25,11 @@ class QuizScoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_quiz_score, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_quiz_score, container, false)
+        binding.viewModel = mainViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
+
 
 }
